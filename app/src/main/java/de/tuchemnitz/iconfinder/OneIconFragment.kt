c08fef46499c3c033a7195c810d99c030948ebc5
Natalie Schubert <schubertn25@gmail.com>
2021-04-21 13:37:27 +0200

    :lipstick: Added GUI fragments

---
+++ app/src/main/java/de/tuchemnitz/iconfinder/OneIconFragment.kt
@@ -0,0 +1,19 @@
+package de.tuchemnitz.iconfinder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class OneIconFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return view
+    }
+}
---
+++ app/src/main/java/de/tuchemnitz/iconfinder/WelcomeFragment.kt
@@ -0,0 +1,23 @@
+package de.tuchemnitz.iconfinder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater?.inflate(R.layout.all_icons_fragment,
+            container, false)
+
+        return view
+    }
+}
