2b254164f6ed23eb13c74d70e39a948381f15806
Natalie Schubert <schubertn25@gmail.com>
2021-04-22 12:46:04 +0200

    :art: Made minor adjustments and added test images

--- app/src/main/java/de/tuchemnitz/iconfinder/MainActivity.kt
+++ app/src/main/java/de/tuchemnitz/iconfinder/MainActivity.kt
@@ -9,3 +9,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+// This makes random Images appear
+/**
+class MainActivity : AppCompatActivity() {
+val rnd = (1..3).random()
+
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
+// setContentView(R.layout.activity_main)
+setContentView(R.layout.one_icon_fragment)
+
+val img = findViewById<View>(R.id.imgRandom) as ImageView
+val str = "img_" + rnd
+img.setImageDrawable(resources.getDrawable(getResourceID(str, "drawable", applicationContext)))
+
+
+}
+
+private fun getResourceID(
+resName: String,
+resType: String?,
+ctx: Context
+): Int {
+val ResourceID: Int = ctx.getResources().getIdentifier(
+resName, resType,
+ctx.getApplicationInfo().packageName
+)
+return if (ResourceID == 0) {
+throw IllegalArgumentException(
+"No resource string found with name $resName"
+)
+} else {
+ResourceID
+}
+}
+
+}
+ **/
